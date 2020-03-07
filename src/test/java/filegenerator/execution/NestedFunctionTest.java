75f27ccf12719cc52ece6feaac6691261ba7eb43
Gildas Lebel <gildas.lebel@expertus.ca>
2020-03-06 23:50:03 -0500

    Add unit tests for nested functions

--- src/test/java/filegenerator/execution/NestedFunctionTest.java
+++ src/test/java/filegenerator/execution/NestedFunctionTest.java
@@ -20,15 +20,44 @@ public class NestedFunctionTest {
     }
 
     @Test
-    public void addValueToVariableSuccess() throws FileGeneratorException {
+    public void addRandValueToTest() throws FileGeneratorException {
         Environnement env = Environnement.getEnvironenement();
 
-        AbstractAST astRoot = TestUtils.parseString("{{{add,$test,(rand,0,50)}}}<<<var,$test>>>");
+        AbstractAST astRoot = TestUtils.parseString("{{{set,$test,50}}}{{{add,$test,(rand,0,50)}}}<<<var,$test>>>");
         Assert.assertNotNull(astRoot);
         astRoot.execute();
 
         long longValue = Long.parseLong(env.getOutput());
-        Assert.assertTrue(longValue > 0 && longValue <= 50);
+        Assert.assertTrue(longValue >= 50 && longValue <= 100);
     }
 
+    @Test
+    public void addValueToVariable() throws FileGeneratorException {
+        Environnement env = Environnement.getEnvironenement();
+
+        AbstractAST astRoot = TestUtils.parseString("{{{add,$test,(add,1,2)}}}<<<var,$test>>>");
+        Assert.assertNotNull(astRoot);
+        astRoot.execute();
+        Assert.assertEquals("3", env.getOutput());
+    }
+
+    @Test
+    public void multipleNestedFunctions() throws FileGeneratorException {
+        Environnement env = Environnement.getEnvironenement();
+
+        AbstractAST astRoot = TestUtils.parseString("{{{add,$test,(add,1,(add,1,(add,1,1)))}}}<<<var,$test>>>");
+        Assert.assertNotNull(astRoot);
+        astRoot.execute();
+        Assert.assertEquals("4", env.getOutput());
+    }
+
+    @Test
+    public void nestedFunctionsInDisplay() throws FileGeneratorException {
+        Environnement env = Environnement.getEnvironenement();
+
+        AbstractAST astRoot = TestUtils.parseString("<<<var,(add,1,(add,1,(add,1,(add,1,1))))>>>");
+        Assert.assertNotNull(astRoot);
+        astRoot.execute();
+        Assert.assertEquals("5", env.getOutput());
+    }
 }
